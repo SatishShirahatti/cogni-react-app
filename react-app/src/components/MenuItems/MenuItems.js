5d115535b151843b764dade27d84491ef9da1120
satish shirahatti <satish.shirahatti@cognizant.com>
2020-06-02 22:13:31 +0200

    added link.

--- react-app/src/components/MenuItems/MenuItems.js
+++ react-app/src/components/MenuItems/MenuItems.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom"
 import { Form } from 'react-bootstrap';
 import { ArrowRight } from 'react-bootstrap-icons';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -38,10 +37,11 @@ return (
 							Menu_Data.Menu_items.map((item,index)=>((
 								<Col sm={12} md={6} key={index}>
 									<div role="listitem">
-										<Link to={item.Link} target="_blank">
+										<a href={item.Link} 
+										rel="noopener noreferrer"  target="_self">
 											{item.Name}<span className="sr-only ng-binding"></span>
 											<ArrowRight style={{ float: 'right' }} color="#4d4e50" size={20} />
-										</Link>
+										</a>
 									</div>
 								</Col>
 
